fb278c2c69231f5c595c43d2e303039d1c495053
Wilber Torres <winftc@gmail.com>
2017-04-18 21:47:12 -0500

    adding sample class

---
+++ src/main/java/org/darkbit/gitflow/GitFlowSample.java
@@ -0,0 +1,11 @@
+package org.darkbit.gitflow;
+
+public class GitFlowSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+
+	}
+
+}
